f44c436a73c65243ed25ca06abe3d9c93cae3a19
Gathoni <janetgathoni910@gmail.com>
2023-08-28 10:20:15 +0300

    Completed

--- question3.py
+++ question3.py
@@ -13,3 +13,14 @@ sentence = "This is a test sentence. This sentence is a test."
 result = word_frequency(sentence)
 print(result)
 '''
+
+def word_frequency(sentence):
+    content = sentence.split()
+    result = [word.strip("?,!.") for word in content]
+    return {word: result.count(word) for word in result}
+    
+sentence = "This is a test sentence. This sentence is a test."
+result = word_frequency(sentence)
+print(result)
+    
+    
