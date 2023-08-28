9a9737c3e2d575c20f44a92a6ef4e4dc4ae18621
Gathoni <janetgathoni910@gmail.com>
2023-08-28 12:38:59 +0300

    Solution

--- question1.py
+++ question1.py
@@ -1,14 +1,29 @@
-''' Question 1: Implement a function is_balanced(expression) that takes a string 
-containing parentheses, curly braces, and square brackets,and determines whether 
-the expression is balanced. 
 
-An expression is considered balanced if each opening bracket has a corresponding closing 
-bracket in the correct order.
+def is_balanced(expression):
+    l = []
+    condition = True
+    
+    
+    if len(expression) % 2 != 0:
+        return False
 
-sample input = 
-
+    for char in expression:
+        if condition:
+            if char in "{([":
+                l.append(char)
+            elif char in "})]":
+                if char == '}':
+                    condition = l.pop() == '{'
+                if char == ']':
+                    condition = l.pop() == '['
+                if char == ')':
+                    condition = l.pop() == '('
+            else:
+                condition = False
+                
+    return condition
+        
 expression1 = "([]{})"
-expression2 = "([)]"
+expression2 = "([)}]"
 print(is_balanced(expression1))  # Output: True
-print(is_balanced(expression2))  # Output: False '''
-
+print(is_balanced(expression2))  # Output: False
